78734d863d28010d72439c5ae478efd7bbc90260
Iván <ivanduran2002@gmail.com>
2023-05-31 09:16:56 +0000

    Cambios interfaz

--- AUT2_03AplicacinFinalAndroid/app/src/main/java/com/example/aut2_03aplicacinfinalandroid/MainActivity.java
+++ AUT2_03AplicacinFinalAndroid/app/src/main/java/com/example/aut2_03aplicacinfinalandroid/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = binding.navView;
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_favorite, R.id.nav_list, R.id.nav_settings)
+                R.id.nav_home, R.id.nav_favorite, R.id.nav_list, R.id.nav_settings, R.id.nav_share, R.id.nav_about)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
